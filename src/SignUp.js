c4131141293f4cdc6731558ee3324d72b77a3727
Justin Diaz <justin.s.diaz@gmail.com>
2021-08-26 17:24:53 -0400

    started media queries

--- src/SignUp.js
+++ src/SignUp.js
@@ -2,37 +2,50 @@ import React, {useState} from 'react';
 import Styled from 'styled-components'
 
 const StyledForm = Styled.div`
-
+    position:relative;
     display:flex;
     flex-direction: column;
     padding : 2.5rem;
     padding-top:5rem;
 
     label{
-
         margin-bottom: 0.8rem;
         font-size: 18px;
         font-weight:600;
 
+        
     }
     
     input{
-
         height: 4rem;
+        width:100%;
+        align-items:center;
         margin-bottom: 2.5rem; 
         border: 1px solid #C7C7C7;
         background-color: #EDEDED;
-
     }
+    .textInput{
+       
+    }
+
 
 `
 const StyledButton = Styled.button`
+    display: block;
     background-color: ${props => props.color};
     color:white;
     border:none;
     height:4rem;
     font-size:18px;
     font-family: 'Poppins', sans-serif;
+    margin-top: 4rem;
+        
+        @media(min-width: 800px){
+            margin-top:0;
+            display:block;
+            width: 12rem;
+
+        }
 
 `
 
@@ -51,13 +64,14 @@ const SignUp = (props) => {
     return (
         <div>
             <StyledForm >
-
-                <label htmlFor="name">Name</label>
-                <input type="text" name="name" value={data.name} onChange={handleChange}/>
-
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" value={data.email} onChange={handleChange}/>
-                
+                <div className="textInput">
+                    
+                        <label className ="input" htmlFor="name">Name</label>
+                        <input type="text" name="name" value={data.name} onChange={handleChange}/>   
+                        <label className="input" htmlFor="email">Email</label>
+                        <input type="email" name="email" value={data.email} onChange={handleChange}/>
+                   
+                 </div>
                 <StyledButton color="#5200FF" >Sign me up</StyledButton>
                 {console.log(data)}
             </StyledForm>
